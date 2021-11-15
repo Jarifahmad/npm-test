7c4b6605e1156ca65cacd2119da118e17da70056
Jarif Ahmad <jarifahmad@yahoo.com>
2021-11-15 22:44:56 +0000

    added "uniq" var

--- bundle.js
+++ bundle.js
@@ -5671,9 +5671,72 @@
 })));
 
 },{}],2:[function(require,module,exports){
+"use strict"
+
+function unique_pred(list, compare) {
+  var ptr = 1
+    , len = list.length
+    , a=list[0], b=list[0]
+  for(var i=1; i<len; ++i) {
+    b = a
+    a = list[i]
+    if(compare(a, b)) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+  list.length = ptr
+  return list
+}
+
+function unique_eq(list) {
+  var ptr = 1
+    , len = list.length
+    , a=list[0], b = list[0]
+  for(var i=1; i<len; ++i, b=a) {
+    b = a
+    a = list[i]
+    if(a !== b) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+  list.length = ptr
+  return list
+}
+
+function unique(list, compare, sorted) {
+  if(list.length === 0) {
+    return list
+  }
+  if(compare) {
+    if(!sorted) {
+      list.sort(compare)
+    }
+    return unique_pred(list, compare)
+  }
+  if(!sorted) {
+    list.sort()
+  }
+  return unique_eq(list)
+}
+
+module.exports = unique
+
+},{}],3:[function(require,module,exports){
 var moment = require('moment'); // require
+var unique = require('uniq');
 
 let myDate = new Date();
 let myCoolDate = moment(myDate).format("LL"); 
-console.log(myCoolDate)
-},{"moment":1}]},{},[2]);
+let myList = [1,2,1,1,1,3,3,4,5,6,4,4,7];
+
+let myUniqueList = unique(myList);
+console.log(myUniqueList)
+},{"moment":1,"uniq":2}]},{},[3]);
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var moment = require('moment'); // require
+var unique = require('uniq');
 
 let myDate = new Date();
 let myCoolDate = moment(myDate).format("LL"); 
-console.log(myCoolDate)
+let myList = [1,2,1,1,1,3,3,4,5,6,4,4,7];
+
+let myUniqueList = unique(myList);
+console.log(myUniqueList)
